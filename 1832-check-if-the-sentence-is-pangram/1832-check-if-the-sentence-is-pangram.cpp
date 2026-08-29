f5ed1382bff571ef1377380ffe2262868cc795ea
Madhav Gadge <madhavgadge01@gmail.com>
2026-08-29 18:00:02 +0530

    Add solution to check if a sentence is a pangram

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+         int freq[26] = {0};
+
+        for (int i = 0; i < sentence.length(); i++) {
+            freq[sentence[i] - 'a']++;
+        }
+        for (int j = 0; j < 26; j++) {
+            if (freq[j] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};

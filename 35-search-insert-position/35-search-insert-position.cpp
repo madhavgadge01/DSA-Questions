08fdd0171843506dbe3b729bd9d9acd427dea47a
Madhav Gadge <madhavgadge01@gmail.com>
2026-08-31 17:07:53 +0530

    Remove unnecessary blank line in searchInsert function

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
           int low = 0;
-          
          int high = nums.size() - 1;
         while(low<=high){
             int mid = low+(high-low)/2;

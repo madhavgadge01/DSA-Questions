b96583edf71da38945c5b4b51aebcc1d76b79463
Madhav Gadge <madhavgadge01@gmail.com>
2026-09-03 19:57:53 +0530

    Remove unnecessary comments from the argumentsLength function

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.cpp
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.cpp
@@ -1,11 +1,5 @@
-/**
- * @param {...(null|boolean|number|string|Array|Object)} args
- * @return {number}
- */
+
 var argumentsLength = function(...args) {
     return args.length;
 };
 
-/**
- * argumentsLength(1, 2, 3); // 3
- */

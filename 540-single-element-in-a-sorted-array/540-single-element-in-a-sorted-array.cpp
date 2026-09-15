1daeca4bab436a42a8bf9130b40352ca16958015
Madhav Gadge <madhavgadge01@gmail.com>
2026-09-15 18:42:47 +0530

    Add implementation of singleNonDuplicate function to find the unique element in a sorted array

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low =0;
+         int high = nums.size() - 1;
+          while(low<=high){
+              int mid = low+(high-low)/2;
+              
+              if ((mid == 0 || nums[mid] != nums[mid - 1]) &&
+                (mid == nums.size() - 1 || nums[mid] != nums[mid + 1])) {
+                return nums[mid];
+            }
+             
+ if (mid % 2 == 0) {
+                if (nums[mid] == nums[mid + 1]) {
+                    low = mid + 2;
+                } else {
+                    high = mid - 2;
+                }
+            }
+
+            else {
+                if (nums[mid] == nums[mid - 1]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+
+          }
+          return -1;
+    }
+};
